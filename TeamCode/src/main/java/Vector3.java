e304701f4e27031f92d5287d1085e39c87cc260a
Chiraag Chakravarthy <beaverduck99@gmail.com>
2019-07-03 23:53:13 -0400

    Forgot Vector3

---
+++ TeamCode/src/main/java/Vector3.java
@@ -0,0 +1,32 @@
+public class Vector3 {
+    private double x, y, r;
+    public Vector3(double x, double y, double r){
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
