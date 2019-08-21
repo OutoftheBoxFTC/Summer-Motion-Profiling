f2521eabb994f32e1b536bec1255c64e0f1cdaf0
Chiraag Chakravarthy <beaverduck99@gmail.com>
2019-08-21 12:49:01 -0400

    Documented Functionality Test

--- TeamCode/src/main/java/opmode/FunctionalityTest.java
+++ TeamCode/src/main/java/opmode/FunctionalityTest.java
@@ -10,7 +10,18 @@ import math.Vector3;
 import math.Vector4;
 import motion.DriveState;
 import state.LogicState;
-
+/**
+ * This class is a raw debug class of all sensors/functionality to test against expected behaviour.
+ * Y: Front left wheel fwd
+ * X: Front right wheel fwd
+ * A: Back left wheel fwd
+ * B: Back right wheel fwd
+ *
+ * When moving robot fwd, left and right increases
+ * When rotating cc, aux and right increases, left decreases
+ *
+ * Gyro starts at 0, increases to 2pi, and returns to 0 after 1 full cc rotation
+ */
 @TeleOp(name = "Functionality Test")
 public class FunctionalityTest extends BasicOpmode {
 
